1e051ea4097e681eb505fc1c37eb075d88114de6
Nurbek <152709750+majidovnurbek@users.noreply.github.com>
2025-02-14 11:49:00 +0500

    rewritten all models

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from api.models import User,OTM,Student,Sponsor,Sponsorship
+from api.models import User,University,Student,Sponsor,StudentSponsor
 
 admin.site.register(User)
-admin.site.register(OTM)
+admin.site.register(University)
 admin.site.register(Student)
 admin.site.register(Sponsor)
-admin.site.register(Sponsorship)
+admin.site.register(StudentSponsor)
--- api/models.py
+++ api/models.py
@@ -1,76 +1,105 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from django.utils.translation import gettext_lazy as _
-
 from api.managers import UserManager
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     ROLE_CHOICES = [
-        ('user', 'User'),
         ('student', 'Student'),
         ('admin', 'Administrator'),
         ('sponsor', 'Sponsor'),
     ]
+
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=150, unique=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
     role = models.CharField(max_length=30, choices=ROLE_CHOICES, null=True, blank=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    is_staff = models.BooleanField(_('staff'), default=True)
-
+    avatar = models.ImageField(upload_to="avatar/student", default="../media/avatar/student/default-student.webp")
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username', 'password']
 
     class Meta:
         verbose_name = 'user'
         verbose_name_plural = 'users'
 
-    def __str__(self):
-        return self.email
 
-
-class OTM(models.Model):
-    name = models.CharField(_('OTM nomi'), max_length=150)
+class University(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'university'
+        verbose_name_plural = 'universities'
+
 
 class Student(models.Model):
-    STUDENT_TYPES = [
-        ('bakalavr', 'Bakalavr'),
-        ('magistr', 'Magistr'),
-    ]
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=150)
-    phone_number = models.CharField(max_length=15)
-    otm = models.ForeignKey(OTM, on_delete=models.CASCADE, related_name='students')
-    created_date = models.DateField(auto_now_add=True)
-    student_type = models.CharField(choices=STUDENT_TYPES, default='bakalavr', max_length=50)
-    contract_amount = models.DecimalField(max_digits=15, decimal_places=2)
+    class StudentTypes(models.TextChoices):
+        BACHELOR = "bachelor"
+        MASTER = "master"
+
+    full_name = models.CharField(max_length=100)
+    degree = models.CharField(max_length=50, choices=StudentTypes.choices)
+    contract_price = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    university = models.ForeignKey("api.University", on_delete=models.CASCADE, related_name="students")
 
     def __str__(self):
         return self.full_name
 
+    class Meta:
+        verbose_name = 'student'
+        verbose_name_plural = 'students'
+
 
 class Sponsor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    organization_name = models.CharField(max_length=255, blank=True, null=True)  # Yur. shaxslar uchun
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    created_date = models.DateField(auto_now_add=True)
+    class StatusChoices(models.TextChoices):
+        NEW = 'new', 'New'
+        IN_PROCESS = 'in_process', 'In process'
+        CONFIRMED = 'confirmed', 'Confirmed'
+        CANCELLED = 'cancelled', 'Cancelled'
+
+    class SponsorStatus(models.TextChoices):
+        JURIDICAL = 'yuridik', 'Yuridik shaxs'
+        INDIVIDUAL = 'jismoniy', 'Jismoniy shaxs'
+
+    sponsor_full_name = models.CharField(max_length=250)
+    student_sponsor = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="sponsors")
+    phone = models.CharField(max_length=30)
+    amount = models.PositiveBigIntegerField()
+    progress = models.CharField(max_length=30, choices=StatusChoices.choices)
+    sponsor_status = models.CharField(max_length=50, choices=SponsorStatus.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    organization_name = models.CharField(max_length=250, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if self.sponsor_status == self.SponsorStatus.INDIVIDUAL:
+            self.organization_name = None  # Jismoniy shaxs bo'lsa, tashkilot nomini o'chiramiz
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.user.first_name or self.user.email
+        return f"{self.full_name} ({self.organization_name if self.organization_name else 'Jismoniy shaxs'}) - {self.amount} UZS"
+
+    class Meta:
+        verbose_name = 'sponsor'
+        verbose_name_plural = 'sponsors'
 
 
-class Sponsorship(models.Model):
-    sponsor = models.ForeignKey(Sponsor, on_delete=models.CASCADE, related_name="sponsorships")
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="sponsorships")
-    amount = models.DecimalField(max_digits=15, decimal_places=2)
-    created_date = models.DateField(auto_now_add=True)
+class StudentSponsor(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    sponsor = models.ForeignKey(Sponsor, on_delete=models.CASCADE)
+    amount = models.PositiveBigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.sponsor} → {self.student} ({self.amount} UZS)"
+        return f"{self.student} - {self.sponsor} - {self.amount} UZS"
+
+    class Meta:
+        verbose_name = 'student sponsor'
+        verbose_name_plural = 'student sponsors'
